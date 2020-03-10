7fa79cfd96565db52f4e7febf7f912bb3775637c
Msangi <msangicode@gmail.com>
2020-03-10 08:10:50 +0300

    Created all posts herein

---
+++ blogi/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
+]
