d893add5b26322bd1f6ab8a410078e7e6719a26f
Msangi <msangicode@gmail.com>
2020-03-13 10:47:40 +0300

    Sytle the blog app, using html and CSS

--- blogi/urls.py
+++ blogi/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('admin/', admin.site.urls),
 ]
--- blogi/views.py
+++ blogi/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts' : posts}
     return render(request, "blogi/post_list.html", stuff_for_frontend)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk);
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blogi/post_detail.html', stuff_for_frontend)
