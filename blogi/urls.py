217905d58f75fb9bc5aea599462a862e5e49666c
Msangi <msangicode@gmail.com>
2020-03-15 19:20:55 +0300

    Add the ability to create drafts and publish them

--- blogi/urls.py
+++ blogi/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('drafts/new', views.post_draft_new, name='post_draft_new'),
+    path('drafts/<int:pk>/', views.post_draft_detail, name='post_draft_detail'),
+    path('drafts/<int:pk>/edit/', views.post_draft_edit, name='post_draft_edit'),
+    path('drafts/<int:pk>/publish', views.publish_draft, name='publish_draft'),
 ]
--- blogi/views.py
+++ blogi/views.py
@@ -1,5 +1,5 @@
 from .models import Post
-from .forms import PostForm
+from .forms import DraftForm, PostForm
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render, redirect
 
@@ -24,7 +24,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit= False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -39,10 +38,52 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         stuff_for_frontend = {'form': form}
     return render(request, 'blogi/post_edit.html', stuff_for_frontend)
+
+def post_draft_list(request):
+    draft_list = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    stuff_for_frontend = {'draft_list': draft_list}
+    return  render(request, 'blogi/post_draft_list.html', stuff_for_frontend)
+
+def post_draft_edit(request, pk):
+    post_draft = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = DraftForm(request.POST, instance = post_draft)
+        if form.is_valid():
+            post_draft = form.save(commit=False)
+            post_draft.author = request.user
+            post_draft.save()
+            return redirect('post_draft_detail',pk=post_draft.pk)
+    else:
+        form = DraftForm(instance=post_draft)
+        stuff_for_frontend = {'post_draft': form}
+    return render(request, 'blogi/post_draft_edit.html', stuff_for_frontend)
+
+def post_draft_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blogi/post_draft_detail.html', stuff_for_frontend)
+
+def post_draft_new(request):
+    if request.method == 'POST':
+        form = DraftForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit= False)
+            post.author = request.user
+            post.save()
+            return redirect('post_draft_detail', pk=post.pk)
+    else:
+        form = DraftForm()
+        stuff_for_frontend = {'post_draft': form}
+        return render(request, 'blogi/post_draft_edit.html', stuff_for_frontend)
+
+def publish_draft(request, pk):
+    post_to_publish = get_object_or_404(Post, pk=pk)
+    post_to_publish.published_date = timezone.now()
+    post_to_publish.save()
+    return redirect('post_detail', pk=pk)
